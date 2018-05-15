2ae1d91cf3bd335a66ecded1600ef61bac64e00c
Kenny Capote <kenny.capote@gmail.com>
2018-05-14 23:40:48 -0400

    gant

--- src/app/pages/gantt/gantt.component.ts
+++ src/app/pages/gantt/gantt.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import "dhtmlx-gantt";
+import {} from "@types/dhtmlxgantt";
 
 @Component({
   selector: 'app-gantt',
   templateUrl: './gantt.component.html',
-  styles: []
+  styles: [
+    `
+    :host{
+        display: block;
+        height: 600px;
+        position: relative;
+        width: 100%;
+    }
+  `]
 })
+
 export class GanttComponent implements OnInit {
+  
+  @ViewChild("gantt_here") ganttContainer: ElementRef;
+
+
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+      gantt.init(this.ganttContainer.nativeElement);
   }
+
+
 
 }
