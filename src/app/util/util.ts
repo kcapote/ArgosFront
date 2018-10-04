66c744bf8545369c0811dff006f0c907fd1367a7
Kerbyt Capote <kerbyt.capote@gmail.com>
2018-10-03 22:17:48 -0300

    cambios

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -2,8 +2,8 @@ import { FormControl } from "@angular/forms";
 
 
 export class Util {
-    //public static URL_SERVER = 'http://180.124.152.20:3001';
-    public static URL_SERVER = 'http://localhost:3001';
+    public static URL_SERVER = 'https://jca-control-backend.herokuapp.com';
+    //public static URL_SERVER = 'http://localhost:3001';
     public static TOKEN = "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVG9la2VuIjp7Il9pZCI6IjVhZDk0NThlZWJhYmRlMDNiNDU4Yjk3MSIsIm5hbWUiOiJLZXJieXQiLCJsYXN0TmFtZSI6IkNhcG90ZSIsImVtYWlsIjoia2VyYnl0LmNhcG90ZUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiIsIl9fdiI6MCwicm9sZSI6IkFETUlOX1JPTEUifSwiaWF0IjoxNTI0MzQ1NzQxLCJleHAiOjE1NTU4ODE3NDF9.A4GejRK_F5lAsOpqNUhzimk-tVS7lgRvX8O7Vpju6fg"    
     public static  URL_TASKS  = `${ Util.URL_SERVER }/task` ;
     public static  URL_TASKS_BY_TYPE  = `${ Util.URL_SERVER }/task/type` ;
